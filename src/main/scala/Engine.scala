78c16fb06ed6a129291e4853e29567311e976a11
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-11-29 14:11:35 +0530

    Update Engine.scala

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -2,11 +2,11 @@ package org.template.regression
 
 import org.apache.predictionio.controller.{EmptyEvaluationInfo, Engine, EngineFactory}
 
-case class Query(vector: Array[Double])
+case class Query(vector: Array[String])
 case class PredictedResult(
-  prediction: Double
+  prediction: String
 )
-case class ActualResult(label: Double)
+case class ActualResult(label: String)
 
 object RegressionEngine extends EngineFactory {
   type Type = Engine[TrainingData, EmptyEvaluationInfo, PreparedData, Query, PredictedResult, ActualResult]
