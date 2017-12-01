3e1f353ec82dd8a4abc1b10c369c8e99b93c9957
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-01 13:15:17 +0530

    Update DataSource.scala

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -23,7 +23,7 @@ class DataSource(ep: EmptyParams)
     val events = selectEvents(sc)
     val rdd = events.map {
       case (_, properties) =>
-        Query(properties.get[String]("vector")) -> ActualResult(properties.get[Double]("label"))
+        Query(properties.get[Array[Double]]("vector")) -> ActualResult(properties.get[Double]("label"))
     }
     val eval = (TrainingData(events), new EmptyEvaluationInfo(), rdd)
     Seq(eval)
@@ -40,7 +40,7 @@ class DataSource(ep: EmptyParams)
       case (entityId, properties) =>
         val fields = Map(
           "vector" -> JArray(List(
-             JString(properties.get[String]("vendor")),JString(properties.get[String]("item")),JString(properties.get[String]("month")),JString(properties.get[String]("day"))
+             JDouble(properties.get[Double]("vendor")),JDouble(properties.get[Double]("item")),JDouble(properties.get[Double]("month")),JDouble(properties.get[Double]("day"))
            )),
 		   "label" -> JDouble(properties.get[Double]("quantity"))
         )
