0f8844f9827e054f205d6fa8f686a218f2925ef2
accentureapis <31919314+accentureapis@users.noreply.github.com>
2017-12-01 18:53:52 +0530

    Update DataSource.scala

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -40,7 +40,7 @@ class DataSource(ep: EmptyParams)
       case (entityId, properties) =>
         val fields = Map(
           "vector" -> JArray(List(
-             JDouble(properties.get[Double]("vendor")),JDouble(properties.get[Double]("item")),JDouble(properties.get[Double]("month")),JDouble(properties.get[Double]("day"))
+             JString(properties.get[Double]("String")),JDouble(properties.get[Double]("item")),JDouble(properties.get[Double]("month")),JDouble(properties.get[Double]("day"))
            )),
 		   "label" -> JDouble(properties.get[Double]("quantity"))
         )
